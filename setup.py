046a35356c07902c41b5a3b34ebcc9025307e5b0
Diogo Braga <a82547@alunos.uminho.pt>
2021-08-21 15:55:14 +0100

    GNN Environment: OS X & Linux - Torch & Torch_Geometric

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 import subprocess
+from sys import platform
 
 # conda-forge
 bashCommand1 = "conda config --add channels conda-forge pytorch ostrokach-forge rusty1s"
@@ -14,8 +15,7 @@ output, error = process.communicate()
 # Instalation requirements
 setup(name='yield_prediction',
       version='1.0',
-      install_requires=['torch==1.9.0',
-                        'networkx==2.2',
+      install_requires=['networkx==2.2',
                         'grakel==0.1.8',
                         'matplotlib==3.3.2',
                         'xlrd==1.2.0',
@@ -28,23 +28,26 @@ setup(name='yield_prediction',
       package_data={'yield_prediction': ['README.md']},
       packages=find_packages())
 
-#setup(name='yield_prediction',
-#      version='1.0',
-#      install_requires=['torch-geometric==1.7.2',
-#                        'torch-spline-conv==1.2.1',
-#                        'torch-cluster==1.5.4',
-#                        'torch-sparse==0.6.10',
-#                        'torch-scatter==2.0.5',
-#                        ],
-#      package_data={'yield_prediction': ['README.md']},
-#      packages=find_packages())
-
 bashCommand3 = "conda install -n yield_prediction rdkit=2021.03.3 -y"
 process = subprocess.Popen(bashCommand3.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
-bashCommand4 = "conda install -n yield_prediction pytorch-geometric -c rusty1s -c conda-forge -y"
-process = subprocess.Popen(bashCommand4.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
+if platform == "linux" or platform == "linux2": # linux
+      bashCommand4 = "pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html -y"
+      process = subprocess.Popen(bashCommand4.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+
+      #bashCommand5 = "pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://pytorch-geometric.com/whl/torch-1.9.0+cu111.html"
+      #process = subprocess.Popen(bashCommand5.split(), stdout=subprocess.PIPE)
+      #output, error = process.communicate()
+      
+elif platform == "darwin": # OS X
+      bashCommand4 = "pip3 install torch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0"
+      process = subprocess.Popen(bashCommand4.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+
+      bashCommand5 = "pip3 install torch-scatter==2.0.8 torch-sparse==0.6.11 torch-cluster==1.5.9 torch-spline-conv==1.2.1 torch-geometric==1.7.2 -f https://pytorch-geometric.com/whl/torch-1.9.0+cpu.html"
+      process = subprocess.Popen(bashCommand5.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
 
 
